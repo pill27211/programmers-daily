13756f22d7803d015950dc02c34e56ad95251a99
pill27211 <120912574+pill27211@users.noreply.github.com>
2026-08-18 19:25:25 +0900

    [level 3] Title: 입국심사, Time: 13.16 ms, Memory: 7.13 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+ll solution(int n, vector<int> times) {
+    ll l(0), r(1e15);
+    while(l <= r)
+    {
+        ll m(l + r >> 1), n_(n);
+        
+        for(ll i{}; i < times.size(); i++)
+            n_ -= m / times[i];
+            
+        if(n_ > 0) l = m + 1;
+        else r = m - 1;
+    }
+    
+    return l;
+}
